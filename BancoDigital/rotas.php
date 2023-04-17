11dbb77a5f856c20936cc88f6676d13a58364e96
Usuario <Usuario@CPD02-M03>
2023-04-17 09:16:50 -0300

    adicionando as rotas

--- BancoDigital/rotas.php
+++ BancoDigital/rotas.php
@@ -3,24 +3,25 @@
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch($url){
+      //   http://localhost:8000/correntista/save
      case '/correntista/save':
         break;
-
+      //   http://localhost:8000/conta/extrato
         case '/conta/extrato':
         break;
-
+      //   http://localhost:8000/conta/pix/enviar
      case '/conta/pix/enviar':
         break;
-
+      //   http://localhost:8000/conta/pix/receber
      case '/conta/pix/receber':
         break;
-
+      //   http://localhost:8000/correntista/entrar
       case '/correntista/entrar':
-        break;   
-        
+        break;
+
         default:
         http_response_code(403);
         break;
-        
-    
+
+
 }
