ec2f1f0af9145e121b7b07d9c3ca3e29370cef68
BrunoPignatari <zemane1312@gmail.com>
2023-04-27 14:43:48 -0300

    concluindo a controller

--- BancoDigital/Controller/CorrentistaController.php
+++ BancoDigital/Controller/CorrentistaController.php
@@ -1,10 +1,20 @@
 <?php
 namespace BancoDigital\Controller;
 
+use BancoDigital\Model\CorrentistaModel;
+
 class CorrentistaController extends Controller {
 	public static function save() 
 	{
+		$json_obj = parent::getJSONFromRequest();
 
+		$model = new CorrentistaModel();
+		$model->id = $json_obj->id;
+		$model->cpf = $json_obj->cpf;
+		$model->data_nasc = $json_obj->data_nasc;
+		$model->senha = $json_obj->senha;
+		
+		$model->save();
 	}
 
 	public static function select() 
