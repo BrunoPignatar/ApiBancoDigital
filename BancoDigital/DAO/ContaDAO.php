5ee1f4029704760408afd817a7720b328ff037da
Bruno Pignatar <zemane1312@gmail.com>
2023-05-15 09:23:08 -0300

    first commit

--- BancoDigital/DAO/ContaDAO.php
+++ BancoDigital/DAO/ContaDAO.php
@@ -1,5 +1,7 @@
 <?php
 namespace BancoDigital\DAO;
+
+use BancoDigital\Model\ContaModel;
 use \PDO;
 
 class ContaDAO extends DAO {
@@ -9,28 +11,68 @@ class ContaDAO extends DAO {
         parent::__construct();      
     }
 
-    public function insert() 
+    public function insert(ContaModel $model) 
     {
+        $sql = "INSERT INTO Conta(tipo, saldo, limite, id_correntista) VALUES (?, ?, ?, ?)";
 
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $model->tipo);
+        $stmt->bindValue(2, $model->saldo);
+        $stmt->bindValue(3, $model->limite);
+        $stmt->bindValue(4, $model->id_correntista);
+        $stmt->execute();
+
+        return $this->conexao->lastInsertId();
     }
 
-    public function update() 
+    public function update(ContaModel $model) 
     {
+        $sql = "UPDATE Conta SET tipo = ?, saldo = ?, limite = ?, id_correntista = ? WHERE id = ?";
 
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $model->tipo);
+        $stmt->bindValue(2, $model->saldo);
+        $stmt->bindValue(3, $model->limite);
+        $stmt->bindValue(4, $model->id_correntista);
+        $stmt->bindValue(5, $model->id);
+
+        return $this->conexao->lastInsertId();
     }
 
     public function select() 
     {
+        $sql = "SELECT c.*,
+        co.nome as nome_conta
+        FROM Conta c
+        JOIN Correntista co ON co.id = c.id_correntista";
+       
 
+        $stmt = $this->conexao->prepare($sql);
+
+        $stmt->execute();
+        
+        return $stmt->fetchAll(PDO::FETCH_CLASS);
     }
 
-    public function selectById() 
+    public function selectById($id) 
     {
-
+        $sql = "SELECT c.*,
+                co.nome as nome_conta
+                FROM Conta c
+                JOIN Correntista co ON co.id = c.id_correntista
+                WHERE id = ?";
+               
+                $stmt = $this->conexao->prepare($sql);
+                $stmt->bindValue(1, $id);    
+                $stmt->execute();
+                return $stmt->fetchObject();
     }
 
-    public function delete() 
+    public function delete($id) 
     {
-
+        $sql = "DELETE FROM Conta WHERE id = ?";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
     }
 }
--- BancoDigital/DAO/CorrentistaDAO.php
+++ BancoDigital/DAO/CorrentistaDAO.php
@@ -1,5 +1,7 @@
 <?php
 namespace BancoDigital\DAO;
+
+use BancoDigital\Model\CorrentistaModel;
 use \PDO;
 
 class CorrentistaDAO extends DAO {
@@ -9,28 +11,63 @@ class CorrentistaDAO extends DAO {
         parent::__construct();      
     }
 
-    public function insert() 
+    public function insert(CorrentistaModel $model) 
     {
+        $sql = "INSERT INTO Correntista(nome, cpf, data_nasc, senha) VALUES (?, ?, ?, ?)";
 
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $model->nome);
+        $stmt->bindValue(2, $model->cpf);
+        $stmt->bindValue(3, $model->data_nasc);
+        $stmt->bindValue(4, $model->senha);
+        $stmt->execute();
+
+        return $this->conexao->lastInsertId();
     }
 
-    public function update() 
+    public function update(CorrentistaModel $model) 
     {
+        $sql = "UPDATE Correntista SET nome = ?, cpf = ?, data_nasc = ?, senha = ? WHERE id = ?";
 
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $model->nome);
+        $stmt->bindValue(2, $model->cpf);
+        $stmt->bindValue(3, $model->data_nasc);
+        $stmt->bindValue(4, $model->senha);
+        $stmt->bindValue(5, $model->id);
+
+        return $this->conexao->lastInsertId();
     }
 
     public function select() 
     {
+        $sql = "SELECT *,
+        FROM Correntista c";
+       
 
+        $stmt = $this->conexao->prepare($sql);
+
+        $stmt->execute();
+        
+        return $stmt->fetchAll(PDO::FETCH_CLASS);
     }
 
-    public function selectById() 
+    public function selectById($id) 
     {
-
+        $sql = "SELECT *,
+        FROM Correntista c";
+       
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $id);    
+        $stmt->execute();
+        return $stmt->fetchObject();
     }
 
-    public function delete() 
+    public function delete($id) 
     {
-
+        $sql = "DELETE FROM Correntista WHERE id = ?";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
     }
 }
